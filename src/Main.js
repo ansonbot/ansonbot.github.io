a84f694ad9a78f22a26a7c8ebf8241f7cb3e5a87
Anson <anson.liang@bluedot.global>
2021-11-09 18:33:44 -0500

    React-ify website

---
+++ src/Main.js
@@ -0,0 +1,47 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <div class="container">
+
+      <div id="main-content" class="transboxbg">
+        <p class="title">
+          <a href="https://ansonbot.github.io" target="_blank" rel="noreferrer" title="">
+            Anson Liang
+          </a>
+        </p>
+        <ul id="main-nav">
+          <li>
+            <a href="https://linkedin.com/in/yiyanliang" target="_blank" rel="noreferrer" title="Anson@LinkedIn">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="https://ansonnet.github.io/geovis/map.html" target="_blank" rel="noreferrer" title="GeoVisualization">
+              Maps
+            </a>
+          </li>
+          <li>
+            <a href="http://www.500px.com/AKB48" target="_blank" rel="noreferrer" title="Anson@500px">
+              500px
+            </a>
+          </li>
+          <li>
+            <a href="http://www.yoyotoronto.com" target="_blank" rel="noreferrer" title="YoYoToronto">
+              YoYoToronto
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div id="footer" class="transboxbg">
+        <p>
+          <a href="mailto:48@hotmail.co.jp" title="48@hotmail.co.jp">Email Anson</a>
+        </p>
+      </div>
+
+    </div>
+  );
+}
+
+export default Main;
