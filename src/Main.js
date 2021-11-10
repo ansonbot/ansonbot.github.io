84343c6902390458703d9e0dd6acae3d8640e6a4
Anson <anson.liang@bluedot.global>
2021-11-10 17:15:34 -0500

    Update styling

--- src/Main.js
+++ src/Main.js
@@ -6,34 +6,41 @@ function Main() {
     <div class="main">
 
       <div id="main-content" class="transboxbg">
-        <p class="main-title">
-          <a href="https://ansonbot.github.io" target="_blank" rel="noreferrer" title="">
+        <p id="main-title">
+          <a href="https://ansonbot.github.io" target="_blank" rel="noreferrer"
+            title="" data-content="Anson Liang">
             Anson Liang
           </a>
         </p>
         <ul id="main-nav">
           <li>
-            <a href="https://linkedin.com/in/yiyanliang" target="_blank" rel="noreferrer" title="Anson@LinkedIn">
+            <a href="https://linkedin.com/in/yiyanliang" target="_blank" rel="noreferrer"
+              title="Anson@LinkedIn" data-content="LinkedIn">
               LinkedIn
             </a>
           </li>
           <li>
-            <a href="https://ansonnet.github.io/geovis/map.html" target="_blank" rel="noreferrer" title="GeoVisualization">
+            <a href="https://ansonnet.github.io/geovis/map.html" target="_blank" rel="noreferrer"
+              title="GeoVisualization" data-content="Maps">
               Maps
             </a>
           </li>
           <li>
-            <a href="http://www.500px.com/AKB48" target="_blank" rel="noreferrer" title="Anson@500px">
+            <a href="http://www.500px.com/AKB48" target="_blank" rel="noreferrer"
+              title="Anson@500px" data-content="500px">
               500px
             </a>
           </li>
           <li>
-            <a href="http://www.yoyotoronto.com" target="_blank" rel="noreferrer" title="YoYoToronto">
+            <a href="http://www.yoyotoronto.com" target="_blank" rel="noreferrer"
+              title="YoYoToronto" data-content="YoYoToronto">
               YoYoToronto
             </a>
           </li>
           <li>
-            <Link to="/example">Test</Link>
+              <Link to="/example">
+                <a href data-content="Test">Test</a>
+              </Link>
           </li>
         </ul>
       </div>
